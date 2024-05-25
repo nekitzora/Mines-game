e7ab6fc09c19a5d97d23b52771f1b8308e8325ee
VasekNaTraktore <168559380+VasekNaTraktore@users.noreply.github.com>
2024-05-25 12:11:31 +0200

    "Removed commented out lines and assigned 'destroying' variable directly in Wall class constructor"

--- game.py
+++ game.py
@@ -6,10 +6,11 @@ class Wall(pygame.sprite.Sprite):
         super().__init__()
         if destroying:
             self.image = pygame.image.load('pic/wall/destroying_wall.png')
-            self.destroying = True
+            # self.destroying = True
         else:
             self.image = pygame.image.load('pic/wall/wall.png')
-            self.destroying = False
+            # self.destroying = False
+        self.destroying = destroying
         self.x = pos_x
         self.y = pos_y
         self.rect = self.image.get_rect()
