3b05121434d5dc2b12e3b9bed7b9f33f11bfb974
VasekNaTraktore <168559380+VasekNaTraktore@users.noreply.github.com>
2024-05-31 20:22:48 +0200

    rebrending 2 chast

--- game.py
+++ game.py
@@ -1,110 +1,63 @@
 import pygame
+# import data
+from data.objects.functions import *
+from data.objects.objects import *
+import draw
+clock = pygame.time.Clock()
 
-from Map import player
-# from classes.wall import destroying_walls as walls_mass
-# from maps.Map import build_map
-from Map import destroying_walls_mass as walls_mass
-from Map import keys
-from classes.key import kluch as keyy
-from Map import enemies
-from classes.door import door
-from classes.bomb import bomb
-# from classes.wall import walls
-from Map import all_walls as walls
-from Map import print_map
-
+set_walls()
+set_enemy()
 
 
 def game(screen):
-    clock = pygame.time.Clock()
-    # pygame.init()
-
-    all_sprites = pygame.sprite.Group()
-    
-
-    
-    
-    
-
-    all_sprites.add(player, enemies)
-    
-    all_sprites.add(door)
-
-    # destroying_wall = build_map()
-
     gaming = True
     while gaming:
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and bomb.placed == False:
-            bomb.spawn(player.rect.x, player.rect.y)
-            all_sprites.add(bomb)
-
-        screen.fill((0, 100, 0)) 
         
-        player.move(key, walls)
-
+        # move
+        klavisha = pygame.key.get_pressed()
+        player.move(klavisha, all_walls)
         for enemy in enemies:
-            enemy.move(walls)
+            enemy.move(all_walls)
+        
+        #draw
+        draw.draw(screen)
+
+
+        #checks
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gaming = False
+                pygame.quit()
+        
+        if klavisha[pygame.K_SPACE] and bomb.placed == False:
+            spawn_bomb(bomb, player.rect.x, player.rect.y)
+            all_sprites.add(bomb)
+            # spawn_key(kluch, player.rect.x, player.rect.y)
 
         if bomb.active:
-            bomb.update()
+            bomb_update(bomb)
         elif not bomb.active:
-            bomb.create_explosions()
-            bomb.draw_explosions(screen)
+            create_explosions(bomb)
+            draw.draw_explosions(bomb, screen)
             for explosion in bomb.explosions:
-                collided_walls = pygame.sprite.spritecollide(explosion, walls, False)
+                collided_walls = pygame.sprite.spritecollide(explosion, all_walls, False)
                 for wall in collided_walls:
-                    if wall in walls_mass:
-                        walls.remove(wall)
-                        walls_mass.remove(wall)
+                    if wall in destroying_walls_mass:
+                        all_walls.remove(wall)
+                        destroying_walls_mass.remove(wall)
                         wall.kill()
             if all(not explosion.active for explosion in bomb.explosions):
                 bomb.explosions.empty()  # Clear all explosion sprites
                 all_sprites.remove(bomb)  # Remove bomb sprite
 
-        all_sprites.draw(screen)
-        print_map(screen)
+        # if bomb.killed_enemy_with_key:
+        #     for enemy in enemies:
+        #         if enemy.have_key:
+        #             kluch = Key(enemy.x, enemy.y)
+        #             keys.add(kluch)
+        #             all_sprites.add(keys)
 
-        screen.blit(player.hp_im[player.hp], (1400, 100))
-        
-        if pygame.sprite.spritecollideany(player, keys):
-            keyy.picked = True
-            door.opened = True
 
-        if not keyy.picked:
-            keys.draw(screen)
-
-        if door.opened:
-            door.change()
-
-        # if pygame.sprite.spritecollideany(player, enemies) and not enemy.player_hit:
-        #     player.hp -= 1
-        #     enemy.player_hit = True
-
-        if not pygame.sprite.spritecollideany(player, enemies):
-            enemy.player_hit = False
-
-        if player.hp == 0:
-            player.dead = True
-
-        
-
-        pygame.display.flip()
-
-        
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gaming = False
-                pygame.quit()
-
-        if pygame.sprite.collide_rect(player, door) and door.opened:
-            gaming = False
-            pygame.quit()
-
-        if player.dead:
-            gaming = False
-            pygame.quit()
-
+        #frame
         if gaming:
-            clock.tick(10)
+            clock.tick(15)
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import pygame
+import game
 
-from game import game
-from Map import build_map
 
 pygame.init()
 pygame.display.set_caption("Game")
-icon = pygame.image.load('pic/main_image.jpeg')
+icon = pygame.image.load('data/pic/main_image.jpeg')
 pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((1550, 850))
 
-build_map(screen)
-game(screen)
-#main_menu()
+game.game(screen)
 
 
 
